0ee4570a6e065aef11124023fc68b5f13c18d02d
Sebastian J. Mielke <sjm@sjmielke.com>
2017-02-27 15:24:14 -0500

    Python vs Rust

---
+++ pcfg-python/pcfg.py
@@ -0,0 +1,329 @@
+import itertools
+from collections import defaultdict, Counter
+import math
+
+from nltk.corpus import treebank
+
+import timeit
+import resource
+
+import json
+
+# Rule :: (int/LHS, [int/RHS or str/terminal])
+# rules :: {int/LHS: [([int/RHS or str/terminal], float/prob)]}
+
+def print_grammar(prules, ntdict):
+	print("{} NTs, {} rules:".format(len(ntdict), len(list(itertools.chain(*prules.values())))))
+	
+	#print(ntdict)
+	return
+	
+	for (lhs, rhss) in prules.items():
+		for (rhs, prob) in rhss:
+			def print_rhs_part(p):
+				if isinstance(p, int):
+					return ntdict[p]
+				else:
+					return p
+			
+			l_out = ntdict[lhs]
+			if isinstance(rhs, tuple):
+				r_out = " ".join([print_rhs_part(r) for r in rhs])
+			else:
+				assert isinstance(rhs, str)
+				r_out = print_rhs_part(rhs)
+			
+			print("{:>8} -> {:15} # {}".format(l_out, r_out, prob))
+
+def intify_prules(grammarbuilder):
+	ntdict = {} # index -> NT
+	rev_ntdict = {} # NT -> index
+	nextindex_d = {'': 1}
+	def treat_nt(w):
+		if w in rev_ntdict:
+			return rev_ntdict[w]
+		else:
+			index = nextindex_d['']
+			ntdict[index] = w
+			rev_ntdict[w] = index
+			nextindex_d[''] = index + 1
+			return index
+	
+	prules = grammarbuilder(treat_nt)
+	
+	return prules, ntdict
+
+def normalize_rules(crisp_rules_iter):
+	# dict-ize
+	crisp_rules = defaultdict(list)
+	for (lhs, rhs) in crisp_rules_iter:
+		crisp_rules[lhs].append(rhs)
+	
+	# normalize to get probs
+	prules = defaultdict(list)
+	for lhs in crisp_rules.keys():
+		Z = float(len(crisp_rules[lhs]))
+		for rhs, count in Counter(crisp_rules[lhs]).items():
+			prules[lhs].append((rhs, float(count) / Z))
+	
+	return prules
+
+# Transform grammar into CNF (but allowing chain rules) for parsing
+def cnfize_grammar(inprules, inntdict):
+	ntdict = {k: v for (k,v) in inntdict.items()}
+	rev_ntdict = {v: k for (k,v) in inntdict.items()}
+	
+	nextindex_d = {'': max(ntdict.keys()) + 1}
+	
+	def binarize(lhs, rhs):
+		if len(rhs) == 2:
+			r1, r2 = rhs
+			return [(lhs, (r1, r2))]
+		elif len(rhs) == 1:
+			# We'll allow chain rules!
+			return [(lhs, (rhs[0],))]
+		else:
+			# let's do... uh... right-branching!
+			r1, r2 = rhs[-2:]
+			pt1 = str(ntdict[r1])
+			pt2 = str(ntdict[r2])
+			pt1 = ("_" if pt1[0] != '_' else "") + pt1
+			pt2 = ("_" if pt2[0] != '_' else "") + pt2
+			newnt = pt1 + pt2
+			if newnt not in rev_ntdict:
+				newindex = nextindex_d['']
+				nextindex_d[''] = newindex + 1
+				ntdict[newindex] = newnt
+				rev_ntdict[newnt] = newindex
+			else:
+				newindex = rev_ntdict[newnt]
+			return binarize(lhs, rhs[:-2] + (newindex,)) + [(newindex, (r1, r2))]
+	
+	outrules = defaultdict(list)
+	for (lhs, rhss) in inprules.items():
+		for (rhs, prob) in rhss:
+			newrules = binarize(lhs, rhs)
+			assert newrules[0][0] == lhs
+			outrules[lhs].append((newrules[0][1], prob))
+			for (l, r) in newrules[1:]:
+				outrules[l].append((r, 1.0))
+	
+	return outrules, ntdict
+
+# Tree :: (str/label, [Tree/child])
+def pp_tree(nt_dict, t):
+	l, cs = t
+	if cs == []:
+		return l
+	else:
+		return nt_dict[l] + "(" + ", ".join(pp_tree(nt_dict, c) for c in cs) + ")"
+
+# Actual CKY parsing
+def cky_parse(cnf_rules, test):
+	# We assume rules are unique.
+	assert len(set(cnf_rules)) == len(cnf_rules)
+	
+	# Build rule dicts
+	# {str/lower: [(int/upper, (float/prob, Tree/parsetree))]}
+	word_to_preterminal = defaultdict(list)
+	# {int/lower: [(int/upper, float/prob)]}
+	nt_chains = defaultdict(list)
+	# {int/rhs1: {int/rhs2: [(int/lhs, float/prob)]}}
+	rhss_to_lhs = defaultdict(list)
+	
+	for (lhs, rhss) in cnf_rules.items():
+		for (rhs, prob) in rhss:
+			r = (lhs, rhs)
+			if len(rhs) == 1:
+				(t,) = rhs
+				if isinstance(t, str):
+					parsetree = (lhs, [(t, [])])
+					word_to_preterminal[t].append((lhs, (math.log(prob), parsetree)))
+				elif isinstance(t, int):
+					nt_chains[t].append((lhs, math.log(prob)))
+			elif len(rhs) == 2:
+				r1, r2 = rhs
+				rhss_to_lhs[(r1, r2)].append((lhs, math.log(prob)))
+			else:
+				raise Exception("Malformed rule " + str(r))
+	
+	# Transform from lists into dicts
+	# {str/lower: {int/upper: (float/prob, Tree/parsetree)}}
+	word_to_preterminal = defaultdict(dict, {k: dict(v) for (k,v) in word_to_preterminal.items()})
+	# {int/lower: {int/upper: float/prob}}
+	nt_chains = defaultdict(dict, {k: dict(v) for (k,v) in nt_chains.items()})
+	
+	# Specific test sentence parsing
+	result = []
+	for s in test:
+		print("parsing:", s)
+		sent = s.split()
+		
+		# CKYchart :: {(int,int): {int/nt: (float/prob, Tree/parsetree)}} # 0-based
+		ckychart = {}
+		
+		# Terminal rules
+		#print("Width: \n1: ", end = '')
+		for (i,w) in enumerate(sent):
+			ws = word_to_preterminal[w]
+			#print(len(ws), "/ ", end = '')
+			ckychart[(i,i)] = ws
+		#print("")
+		
+		# Higher levels
+		for width in range(2, len(sent) + 1):
+			for i in range(0, len(sent) - width + 1):
+				ckychart[(i, i + width - 1)] = {}
+				cell = ckychart[(i, i + width - 1)]
+				for split in range(i + 1, i + width):
+					# Generate new candidates by combining two lower cells
+					#print(" ".join(sent[i:split]), "|", " ".join(sent[split:i+width]))
+					for (r1, (pr1, pt1)) in ckychart[(i, split - 1)].items():
+						for (r2, (pr2, pt2)) in ckychart[(split, i + width - 1)].items():
+							for (lhs, prob) in rhss_to_lhs[(r1,r2)]:
+								new_prob = pr1 + pr2 + prob
+								if lhs not in cell or cell[lhs][0] < new_prob:
+									new_ptree = (lhs, [pt1, pt2])
+									cell[lhs] = (new_prob, new_ptree)
+					# Generate new candidates by applying chain rules
+					got_new = True
+					while got_new:
+						got_new = False
+						for (reached_nt, (lower_prob, lower_ptree)) in list(cell.items()):
+							for (lhs, cr_prob) in nt_chains[reached_nt].items():
+								new_prob = cr_prob + lower_prob
+								if lhs not in cell or cell[lhs][0] < new_prob:
+									got_new = True
+									new_ptree = (lhs, [lower_ptree])
+									cell[lhs] = (new_prob, new_ptree)
+			#print("{}: ".format(width),end="")
+			#print(" / ".join([str(len(ckychart[(i, i + width - 1)])) for i in range(0, len(sent) - width + 1)]))
+		
+		#print("")
+		result.append((s, ckychart[(0, len(sent) - 1)]))
+	
+	return result
+
+# Timing foo
+def get_time():
+	return (timeit.default_timer(), resource.getrusage(resource.RUSAGE_SELF)[0])
+def diff_time(old):
+	walltime = timeit.default_timer() - old[0]
+	cputime = resource.getrusage(resource.RUSAGE_SELF)[0] - old[1]
+	print("Operation took {:5.2f} (walltime) / {:5.2f} (cputime) seconds.".format(walltime, cputime))
+def do_time(f, *p, **pp):
+	t = get_time()
+	res = f(*p, **pp)
+	diff_time(t)
+	return res
+
+# Get example data
+def bananaset():
+	s = """
+		S -> NP
+		NP -> DET NN
+		NP -> DET JJ NN
+		DET -> 'the'
+		DET -> 'the'
+		DET -> 'a'
+		DET -> 'an'
+		JJ -> 'fresh'
+		NN -> 'apple'
+		NN -> 'orange'
+		"""
+	
+	def parse_handwritten_grammar(treat_nt):
+		def getrule(l):
+			[lhs, rhs] = l.split("->")
+			lhs = treat_nt(lhs.strip())
+			rhs = tuple([w[1:-1] if w[0] == "'" and w[-1] == "'" else treat_nt(w) for w in rhs.strip().split()])
+			return (lhs, rhs)
+		
+		return normalize_rules((getrule(l) for l in s.splitlines() if l.strip() != ""))
+	
+	rules, ntdict = intify_prules(parse_handwritten_grammar)
+	test = ["the fresh apple", "the fresh banana"]
+	return (rules, ntdict), test
+
+def ptb_wsj_set():
+	trainsize = 2000
+	testsize = 100
+	
+	ts = list(itertools.chain(*(treebank.parsed_sents(fid) for fid in treebank.fileids())))[:trainsize + testsize]
+	testsents = [" ".join(s.leaves()) for s in ts[trainsize:]]
+	
+	with open("/tmp/trees.json", 'w') as f:
+		lins = []
+		for t in ts[:trainsize]:
+			def lin(t):
+				if isinstance(t, str):
+					label = t
+					cs = []
+				else:
+					label = t.label()
+					cs = t
+				
+				if '"' in label:
+					raise Exception("»\"« occuring in data! :O")
+				
+				return "{\"label\": \"" + label + "\", \"children\": [" + ",".join([lin(c) for c in cs]) + "]}"
+			lins.append(lin(t))
+		print("[", ",\n".join(lins), "]", file = f)
+	
+	with open("/tmp/test.txt", 'w') as f:
+		for s in testsents:
+			print(s, file = f)
+	
+	ts = ts[:trainsize]
+	
+	def parse_treebank_grammar(treat_nt):
+		def getrules(t):
+			lhs = treat_nt(t.label())
+			rhs = []
+			for x in t:
+				if isinstance(x, str):
+					rhs.append(x)
+				else:
+					rhs.append(treat_nt(x.label()))
+					for r in getrules(x):
+						yield r
+			yield (lhs, tuple(rhs))
+		
+		return normalize_rules(itertools.chain(*(getrules(t) for t in ts)))
+	
+	rules, ntdict = intify_prules(parse_treebank_grammar)
+	#test = ["the old man", "something was .", "something was at the stock market today .", "Pierre Vinken , 61 years old , will join the board as a nonexecutive director Nov. 29 ."]
+	return (rules, ntdict), testsents
+
+if __name__ == "__main__":
+	#print("Reading in grammar from string:")
+	#(unbin_rules, unbin_nt), test = do_time(bananaset)
+	
+	print("Reading in PTB-WSJ sample:")
+	(unbin_rules, unbin_nt), test = do_time(ptb_wsj_set)
+	
+	print_grammar(unbin_rules, unbin_nt)
+	
+	print("\nBinarizing grammar:")
+	cnf_rules, cnf_nt = do_time(cnfize_grammar, unbin_rules, unbin_nt)
+	print_grammar(cnf_rules, cnf_nt)
+	
+	print("\nCKY parsing:")
+	parses = do_time(cky_parse, cnf_rules, test)
+	for s, cell in parses:
+		if cell != {}:
+			print("{}".format(s))
+			got_s = False
+			usablecell = list(filter(lambda t: cnf_nt[t[0]][0] != '_', sorted(cell.items(), key = lambda x: x[1], reverse = True)))
+			for (n, (p, ptree)) in usablecell[:10]:
+				print("\t{:15} ({:1.20f}) -> {}".format(cnf_nt[n], math.exp(p), pp_tree(cnf_nt, ptree)))
+				if cnf_nt[n] == "S":
+					got_s = True
+			if len(usablecell) > 10:
+				print("\t...")
+				if not got_s:
+					for (n, (p, ptree)) in sorted(cell.items(), key = lambda x: x[1], reverse = True):
+						if cnf_nt[n] == "S":
+							print("\t{:15} ({:1.20f}) -> {}".format(cnf_nt[n], math.exp(p), pp_tree(cnf_nt, ptree)))
+		else:
+			print("{}\n".format(s))
---
+++ pcfg-rust/src/main.rs
@@ -0,0 +1,444 @@
+use std::collections::HashMap;
+use std::f64; // log and exp
+use std::fs::File;
+use std::io::prelude::*;
+
+extern crate json;
+extern crate fnv;
+
+fn reverse_bijection<V: Clone + Eq + std::hash::Hash, K: Clone + Eq + std::hash::Hash>(indict: &HashMap<K, V>) -> HashMap<V, K> {
+    let mut outdict: HashMap<V, K> = HashMap::new();
+    for item in indict {
+        let (k, v) = item;
+        assert!(outdict.insert(v.clone(), k.clone()).is_none())
+    }
+    outdict
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+enum RHS {
+    Terminal(String),
+    NTs(Vec<usize>),
+    CNFBin(usize, usize),
+    CNFChain(usize)
+}
+
+#[derive(Debug)]
+struct Rule {
+    lhs: usize,
+    rhs: RHS
+}
+
+#[derive(Debug, Clone)]
+enum ParseTree<'a> {
+    InnerNode {
+        label: usize,
+        children: Vec<ParseTree<'a>>
+    },
+    TerminalNode {
+        label: &'a str
+    }
+}
+
+fn pp_tree<'a>(ntdict: &HashMap<usize, String>, t: &ParseTree<'a>) -> String {
+    match t {
+        &ParseTree::TerminalNode { label } => label.to_string(),
+        &ParseTree::InnerNode { label, ref children } => {
+            let cs = children.iter().map(|c| pp_tree(ntdict, &c)).collect::<Vec<_>>().join(", ");
+            ntdict[&label].clone() + "(" + &cs + ")"
+        }
+    }
+}
+
+fn treat_nt(rev_ntdict: &mut HashMap<String, usize>, nt: &str) -> usize {
+    match rev_ntdict.get(nt) {
+        Some(&i) => i,
+        None => {
+            let next_value = rev_ntdict.values().max().unwrap_or(&0) + 1;
+            //println!("{} ~ {}", next_value, nt);
+            rev_ntdict.insert(nt.to_string(), next_value);
+            next_value
+        }
+    }
+}
+
+fn normalize_grammar<'a, I>(rulelist: I) -> HashMap<usize, HashMap<RHS, f64>> where I: Iterator<Item=&'a Rule> {
+    let mut lhs_to_rhs_count: HashMap<usize, HashMap<RHS, usize>> = HashMap::new();
+    
+    for r in rulelist {
+        // First insert new inner map if necessary
+        if !lhs_to_rhs_count.contains_key(&r.lhs) {
+            lhs_to_rhs_count.insert(r.lhs.clone(), HashMap::new());
+        }
+        // Then unwrap.
+        let ref mut innermap = lhs_to_rhs_count.get_mut(&r.lhs).unwrap();
+        let newcount = match innermap.get(&r.rhs) {
+            Some(&count) => count + 1,
+            None => 1
+        };
+        innermap.insert(r.rhs.clone(), newcount);
+    }
+    
+    let mut lhs_to_rhs_prob: HashMap<usize, HashMap<RHS, f64>> = HashMap::new();
+    
+    for (lhs, rhsdict) in lhs_to_rhs_count {
+        let mut innermap = HashMap::new();
+        let z: usize = rhsdict.values().sum();
+        let z: f64 = z as f64;
+        for (rhs, count) in rhsdict {
+            innermap.insert(rhs, (count as f64) / z);
+        }
+        
+        lhs_to_rhs_prob.insert(lhs, innermap);
+    }
+    
+    lhs_to_rhs_prob
+}
+
+fn print_grammar(rules: &HashMap<usize, HashMap<RHS, f64>>, ntdict: &HashMap<usize, String>) {
+    let numrules: usize = rules.values().map(|d| d.len()).sum();
+    println!("{} NTs, {} rules.", ntdict.len(), numrules);
+    
+    if true {
+        println!("{:?}", ntdict);
+        
+        for (ref lhs, ref rhsdict) in rules {
+            for (ref rhs, ref prob) in *rhsdict {
+                match *rhs {
+                    &RHS::Terminal(ref s) => println!("{:>15} -> {:<15} # {:1.10}", &ntdict[*lhs], s, prob),
+                    &RHS::NTs(ref nts) => {
+                        let ntlist: Vec<String> = nts.iter().map(|n| ntdict.get(n).unwrap().to_string()).collect();
+                        println!("{:>15} -> {:<15} # {:1.10}", ntdict[*lhs], ntlist.join(" "), prob)
+                    }
+                    &RHS::CNFBin(ref r1, ref r2) => {
+                        println!("{:>15} -> {:<15} # {:1.10}", ntdict[*lhs], format!("{} {}", &ntdict[r1], &ntdict[r2]), prob)
+                    }
+                    &RHS::CNFChain(ref r) => {
+                        println!("{:>15} -> {:<15} # {:1.10}", ntdict[*lhs], &ntdict[r], prob)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn cnfize_grammar(in_rules: &HashMap<usize, HashMap<RHS, f64>>, ntdict: &HashMap<usize, String>) -> (HashMap<usize, HashMap<RHS, f64>>, HashMap<usize, String>) {
+    let mut rev_ntdict: HashMap<String, usize> = reverse_bijection(&ntdict);
+    
+    fn binarize(ntdict: &HashMap<usize, String>, rev_ntdict: &mut HashMap<String, usize>, lhs: usize, rhs: &[usize]) -> Vec<Rule> {
+        if rhs.len() > 2 {
+            // right-branching
+            let ref rest = rhs[1..rhs.len()];
+            let rest_names: Vec<String> = rest.iter().map(|i| ntdict[i].to_string()).collect();
+            let newnt_str = "_".to_string() + &rest_names.join("_");
+            let newnt_int = treat_nt(rev_ntdict, &newnt_str);
+            //println!("{} ~> {}", newnt_str, newnt_int);
+            
+            let mut result: Vec<Rule> = binarize(ntdict, rev_ntdict, newnt_int, rest);
+            //println!("{:?}", result);
+            
+            // root rule has to go last to be popped!
+            result.push(Rule { lhs: lhs, rhs: RHS::CNFBin(rhs[0], newnt_int) });
+            result
+        } else if rhs.len() == 2 {
+            // identity
+            vec![Rule { lhs: lhs, rhs: RHS::CNFBin(rhs[0], rhs[1]) }]
+        } else if rhs.len() == 1 {
+            vec![Rule { lhs: lhs, rhs: RHS::CNFChain(rhs[0]) }]
+        } else {
+            panic!("Nullary rule detected!")
+        }
+    }
+    
+    let mut cnf_rules: HashMap<usize, HashMap<RHS, f64>> = HashMap::new();
+    let mut new_rules_tmp: Vec<Rule> = Vec::new(); // all have prob 1.0
+    
+    for (lhs, rhsdict) in in_rules {
+        let mut innermap: HashMap<RHS, f64> = HashMap::new();
+        for (rhs, &prob) in rhsdict {
+            match rhs {
+                &RHS::Terminal(_) => assert!(innermap.insert(rhs.clone(), prob).is_none()),
+                &RHS::CNFBin(_, _) => panic!("Already partially CNFized!"),
+                &RHS::CNFChain(_) => panic!("Already partially CNFized!"),
+                &RHS::NTs(ref nts) => {
+                    let mut newrules = binarize(&ntdict, &mut rev_ntdict, lhs.clone(), nts);
+                    let Rule { lhs: lhs_, rhs: rhs_ } = newrules.pop().unwrap();
+                    assert!(*lhs == lhs_);
+                    assert!(innermap.insert(rhs_, prob).is_none());
+                    new_rules_tmp.extend(newrules)
+                }
+            }
+        }
+        assert!(cnf_rules.insert(lhs.clone(), innermap).is_none())
+    }
+    
+    for Rule { lhs, rhs } in new_rules_tmp {
+        // First insert new inner map if necessary
+        if !cnf_rules.contains_key(&lhs) {
+            cnf_rules.insert(lhs, HashMap::new());
+        }
+        // Then unwrap.
+        cnf_rules.get_mut(&lhs).unwrap().insert(rhs, 1.0);
+    }
+    
+    (cnf_rules, reverse_bijection(&rev_ntdict))
+}
+
+fn cky_parse<'a>(cnf_rules: &'a HashMap<usize, HashMap<RHS, f64>>, sents: &Vec<String>) -> Vec<HashMap<usize, (f64, ParseTree<'a>)>> {
+    // Build helper dicts for quick access. All are bottom-up in the parse.
+    let mut word_to_preterminal: HashMap<String, Vec<(usize, (f64, ParseTree))>> = HashMap::new();
+    let mut nt_chains: HashMap<usize, Vec<(usize, f64)>> = HashMap::new();
+    let mut rhss_to_lhs: HashMap<(usize, usize), Vec<(usize, f64)>> = HashMap::new();
+    
+    for (lhs, rhsdict) in cnf_rules {
+        for (rhs, prob) in rhsdict {
+            let logprob = prob.ln();
+            match rhs {
+                &RHS::Terminal(ref s) => {
+                    if !word_to_preterminal.contains_key(s) {
+                        word_to_preterminal.insert(s.clone(), Vec::new());
+                    }
+                    let tree = ParseTree::TerminalNode { label: s };
+                    let tree = ParseTree::InnerNode { label: *lhs, children: vec![tree] };
+                    word_to_preterminal.get_mut(s).unwrap().push((*lhs, (logprob, tree)))
+                }
+                &RHS::CNFChain(ref r) => {
+                    if !nt_chains.contains_key(r) {
+                        nt_chains.insert(r.clone(), Vec::new());
+                    }
+                    nt_chains.get_mut(&r).unwrap().push((*lhs, logprob))
+                }
+                &RHS::CNFBin(ref r1, ref r2) => {
+                    if !rhss_to_lhs.contains_key(&(*r1, *r2)) {
+                        rhss_to_lhs.insert((r1.clone(), r2.clone()), Vec::new());
+                    }
+                    rhss_to_lhs.get_mut(&(*r1, *r2)).unwrap().push((*lhs, logprob))
+                }
+                _ => panic!("Trying to use un-CNFized grammar!")
+            }
+        }
+    }
+    
+    let mut results: Vec<HashMap<usize, (f64, ParseTree<'a>)>> = Vec::new();
+    
+    for raw_sent in sents {
+        println!("parsing: {}", raw_sent);
+        
+        // Tokenize
+        let sent: Vec<&str> = raw_sent.split(" ").collect();
+        
+        // Now populate a chart (0-based indexing)!
+        let mut ckychart: HashMap<(usize, usize), HashMap<usize, (f64, ParseTree)>> = HashMap::new();
+        
+        // Populate leafs
+        for (i,w) in sent.iter().enumerate() {
+            // TODO actually could just break if we don't recognize terminals :D
+            let terminals: HashMap<usize, (f64, ParseTree)> = match word_to_preterminal.get(*w) {
+                Some(prets) => prets.iter().map(|x| x.clone()).collect(),
+                None => HashMap::new()
+            };
+            ckychart.insert((i, i), terminals);
+            //println!("{}, {:?}", i, ckychart[&(i,i)]);
+        }
+        
+        // Populate inner cells
+        for width in 2 .. sent.len() + 1 {
+            for start in 0 .. sent.len() + 1 - width {
+                let end = start + width;
+                let mut cell: HashMap<usize, (f64, ParseTree)> = HashMap::new();
+                
+                // get new ones from bin rules
+                for sp in start + 1 .. end {
+                    //println!("{} | {}", sent[start..sp].join(" "), sent[sp..end].join(" "));
+                    for (r1, &(pr1, ref pt1)) in &ckychart[&(start, sp - 1)] {
+                        for (r2, &(pr2, ref pt2)) in &ckychart[&(sp, end - 1)] {
+                            if let Some(m) = rhss_to_lhs.get(&(*r1,*r2)) {
+                                for &(lhs, prob) in m {
+                                    let new_prob = pr1 + pr2 + prob;
+                                    let tree = ParseTree::InnerNode {label: lhs, children: vec![pt1.clone(), pt2.clone()] };
+                                    match cell.get(&lhs) {
+                                        None => {
+                                            cell.insert(lhs, (new_prob, tree));
+                                        }
+                                        Some(&(old_prob, _)) => if old_prob < new_prob {
+                                            cell.insert(lhs, (new_prob, tree));
+                                        }
+                                    };
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                // get new ones from chain rules
+                let mut got_new = true;
+                while got_new {
+                    got_new = false;
+                    let clonecell = cell.clone();
+                    let celllist: Vec<_> = clonecell.iter().collect();
+                    for (reached_nt, &(lower_prob, ref lower_ptree)) in celllist {
+                        if let Some(v) = nt_chains.get(reached_nt) {
+                            for &(lhs, cr_prob) in v {
+                                let new_prob = cr_prob + lower_prob;
+                                let tree = ParseTree::InnerNode {label: lhs, children: vec![lower_ptree.clone()]};
+                                match cell.get(&lhs) {
+                                    None => {
+                                        cell.insert(lhs, (new_prob, tree));
+                                    }
+                                    Some(&(old_prob, _)) => if old_prob < new_prob {
+                                        cell.insert(lhs, (new_prob, tree));
+                                    }
+                                };
+                            }
+                        }
+                    }
+                }
+                
+                assert!(ckychart.insert((start, end - 1), cell).is_none())
+            }
+        }
+        
+        results.push(ckychart[&(0, sent.len() - 1)].clone())
+    }
+    
+    results
+}
+
+fn bananaset() -> ((Vec<Rule>, HashMap<usize, String>), Vec<String>) {
+    let s = "
+        S -> NP
+        NP -> DET NN
+        NP -> DET JJ NN
+        DET -> 'the'
+        DET -> 'the'
+        DET -> 'a'
+        DET -> 'an'
+        JJ -> 'fresh'
+        NN -> 'apple'
+        NN -> 'orange'
+        ";
+    
+    let mut rulelist: Vec<Rule> = Vec::new();
+    let mut rev_ntdict: HashMap<String, usize> = HashMap::new();
+    
+    for l in s.lines() {
+        let l: &str = l.trim();
+        if !l.is_empty() {
+            let ruleparts: Vec<&str> = l.split(" -> ").collect();
+            assert!(ruleparts.len() == 2);
+            let rule = Rule { lhs: treat_nt(&mut rev_ntdict, ruleparts[0]), rhs:
+                match ruleparts[1].chars().next() {
+                    Some('\'') => {
+                        let mut trimmed_rhs = ruleparts[1][1..].to_string();
+                        let newlen = trimmed_rhs.len() - 1;
+                        trimmed_rhs.truncate(newlen);
+                        RHS::Terminal(trimmed_rhs)
+                    },
+                    Some(_) => { 
+                        let mut nts: Vec<usize> = Vec::new();
+                        for p in ruleparts[1].split(" ") {
+                            nts.push(treat_nt(&mut rev_ntdict, p))
+                        }
+                        RHS::NTs(nts)
+                    },
+                    None => panic!("Empty LHS!"),
+                }
+            };
+            rulelist.push(rule);
+        }
+    }
+    
+    let ntdict = reverse_bijection(&rev_ntdict);
+    
+    ((rulelist, ntdict), vec!["the fresh apple".to_string(), "the fresh banana".to_string()])
+}
+
+fn ptbset() -> ((Vec<Rule>, HashMap<usize, String>), Vec<String>) {
+    let mut f = File::open("/tmp/trees.json").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+    
+    let parsed = json::parse(&s).unwrap();
+    
+    let mut rulelist: Vec<Rule> = Vec::new();
+    let mut rev_ntdict: HashMap<String, usize> = HashMap::new();
+    
+    fn getrules(t: &json::JsonValue, rulelist: &mut Vec<Rule>, rev_ntdict: &mut HashMap<String, usize>) {
+        let l = t["label"].as_str().unwrap();
+        let mut cids: Vec<usize> = Vec::new();
+        // preterminal case
+        if t["children"][0]["children"][0].is_null() {
+            let s = t["children"][0]["label"].as_str().unwrap().to_string();
+            rulelist.push(Rule { lhs: treat_nt(rev_ntdict, l), rhs: RHS::Terminal(s) });
+        }
+        // other case: NTs
+        else {
+            for c in t["children"].members() {
+                let s = c["label"].as_str().unwrap();
+                cids.push(treat_nt(rev_ntdict, s));
+                getrules(c, rulelist, rev_ntdict);
+            }
+            rulelist.push(Rule { lhs: treat_nt(rev_ntdict, l), rhs: RHS::NTs(cids) });
+        }
+    }
+    
+    for t in parsed.members() {
+        getrules(t, &mut rulelist, &mut rev_ntdict);
+    }
+    
+    let ntdict = reverse_bijection(&rev_ntdict);
+    
+    // load test sents
+    let mut f = File::open("/tmp/test.txt").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+    let testsents: Vec<&str> = s.lines().collect();
+    let testsents: Vec<String> = testsents.iter().map(|s| s.to_string()).collect();
+    
+    ((rulelist, ntdict), testsents)
+}
+
+fn main() {
+    let ((rulelist, ntdict), testsents) = ptbset();
+    let rules = normalize_grammar(rulelist.iter());
+    //print_grammar(&rules, &ntdict);
+    println!("Now CNFing!");
+    let (cnf_rules, cnf_ntdict) = cnfize_grammar(&rules, &ntdict);
+    //println!("What came out?");
+    //print_grammar(&cnf_rules, &cnf_ntdict);
+    //println!("try printing original again");
+    //print_grammar(&rules, &ntdict);
+    //println!("try printing cnfized again");
+    //print_grammar(&cnf_rules, &cnf_ntdict);
+    
+    println!("Now parsing!");
+    let parses = cky_parse(&cnf_rules, &testsents);
+    
+    for (s, cell) in testsents.iter().zip(parses.iter()) {
+        if !cell.is_empty() {
+            println!("{}", s);
+            let mut got_s = false;
+            let mut usablecell: Vec<_> = cell.iter().filter(|&(n, _)| cnf_ntdict[n].chars().next().unwrap() != '_').collect();
+            usablecell.sort_by(|&(_, &(p1, _)), &(_, &(p2, _))| p2.partial_cmp(&p1).unwrap_or(std::cmp::Ordering::Equal));
+            for &(n, &(p, ref ptree)) in usablecell.iter().take(10) {
+                println!("\t{:15} ({:1.20}) -> {}", cnf_ntdict[n], p.exp(), pp_tree(&cnf_ntdict, ptree));
+                if cnf_ntdict[n] == "S" {
+                    got_s = true
+                }
+            }
+            if usablecell.len() > 10 {
+                println!("\t...");
+                if !got_s {
+                    for (n, &(p, ref ptree)) in usablecell {
+                        if cnf_ntdict[n] == "S" {
+                            println!("\t{:15} ({:1.20}) -> {}", cnf_ntdict[n], p.exp(), pp_tree(&cnf_ntdict, ptree))
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            println!("{}\n", s);
+        }
+    }
+}
